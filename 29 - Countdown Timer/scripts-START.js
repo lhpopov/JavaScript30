343420d541e99261500272d67602985c71d46fe8
Lyubomir Popov <lhpopov@yahoo.com>
2018-10-11 00:10:23 +0300

    Finished countdown challenge for day 29.

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,4 +1,6 @@
-const timerDisplay = document.querySelector('.display__time-left');
+const timerDisplay = document.querySelector('.display__time-left'),
+	endTimerDisplay = document.querySelector('.display__end-time'),
+	buttons = document.querySelectorAll('[data-time]');
 let timerIntervalId;
 
 function timer(seconds){
@@ -11,6 +13,7 @@ function timer(seconds){
  		then = now + (seconds * 1000);
  	
 	displayTimeLeft(seconds);
+	displayEndTime(then);
 	
  	timerIntervalId = setInterval(() => {
  		const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -30,8 +33,27 @@ function displayTimeLeft(seconds){
 		display = `${mins}:${remainingSecs < 10 ? '0' : ''}${remainingSecs}`;
 
 	timerDisplay.textContent = display;
+	document.title = display;
 }
 
 function displayEndTime(timestamp){
-	const end = new Date(timestamp);
+	const end = new Date(timestamp),
+		hours = end.getHours(),
+		mins = end.getMinutes();
+	
+	endTimerDisplay.textContent = `Be Back At ${hours > 12 ? hours - 12 : hours}:${mins < 10 ? '0' : ''}${mins}`;
 }
+
+function startTimer(){
+	const seconds = parseInt(this.dataset.time);
+
+	timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e){
+	e.preventDefault();
+	const mins = this.minutes.value;
+	timer(mins * 60);
+	this.reset();
+});
